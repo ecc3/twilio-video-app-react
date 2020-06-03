f59c02640fe9e4ecbeceaf767122b88690dc48ec
Anton Forbes <anton.forbes@airelogic.com>
2020-06-03 14:57:39 +0100

    clinician details

--- src/components/Clinician/ClinicianRoom.tsx
+++ src/components/Clinician/ClinicianRoom.tsx
@@ -6,6 +6,7 @@ import LocalVideoPreview from '../LocalVideoPreview/LocalVideoPreview';
 import axios from 'axios';
 import { AireRoom } from '../Room/AireRoom';
 import Video, { ConnectOptions, LocalTrack } from 'twilio-video';
+import { usePatientService } from '../Patient/PatientDetails';
 
 interface IClinicianRoomProps {
   slotId: string;
@@ -19,15 +20,18 @@ export default function ClinicianRoom(props: IClinicianRoomProps) {
   const { isConnecting, connect } = useVideoContext();
   const roomState = useRoomState();
 
+  const { patient, getPatient } = usePatientService();
+
   useEffect(() => {
     if (props.slotId !== '') {
       fetch(`https://rgqra2u25c.execute-api.eu-west-2.amazonaws.com/dev/v2/token?slot=${props.slotId}&user=host`)
         .then(res => res.json())
         .then(result => {
           connect(result.token);
+          getPatient(result.subjectName);
         });
     }
   }, [props.slotId]);
 
-  return <div>{roomState != 'disconnected' && <AireRoom />}</div>;
+  return <div>{roomState != 'disconnected' && <AireRoom patientDetails={patient} />}</div>;
 }
